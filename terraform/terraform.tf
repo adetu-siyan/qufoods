d49a23795cb92dcc5b726c8a59ba4eb7f276fc1d
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2026-08-02 01:26:41 +0100

    Remote backend implementation

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.7"
 
+  backend "s3" {
+        bucket = "qufoods-terraform-state"
+        key    = "qufoods/lambda/terraform.tfstate"
+        region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
